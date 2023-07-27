b7da580515f017f95c96c359aa4e615f12cc708f
Denis <den.sautner@mail.ru>
2023-07-27 19:00:26 +0600

    Finished file hw3.js

--- hw3.js
+++ hw3.js
@@ -42,40 +42,24 @@ let monthNumber = prompt('Введите номер месяца в году');
 
 switch (Number(monthNumber)) {
     case 1:
-        console.log('Зима');
-        break;
     case 2:
+    case 12:
         console.log('Зима');
         break;
     case 3:
-        console.log('Весна');
-        break;
     case 4:
-        console.log('Весна');
-        break;
     case 5:
         console.log('Весна');
         break;
     case 6:
-        console.log('Лето');
-        break;
     case 7:
-        console.log('Лето');
-        break;
     case 8:
         console.log('Лето');
         break;
     case 9:
-        console.log('Осень');
-        break;
     case 10:
-        console.log('Осень');
-        break;
     case 11:
         console.log('Осень');
-        break;
-    case 12:
-        console.log('Зима');
         break;
 
     default:
